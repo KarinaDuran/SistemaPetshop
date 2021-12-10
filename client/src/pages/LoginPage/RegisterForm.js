b7c56fc20e72ec226ccf12b728619ed6cd2f1ff1
Arthur Castilho <castartx@gmail.com>
2021-12-09 22:10:30 -0300

    Tela de cadastro

---
+++ client/src/pages/LoginPage/RegisterForm.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import * as yup from 'yup';
+import Axios from 'axios';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material';
+import PetsIcon from '@mui/icons-material/Pets';
+
+const theme = createTheme();
+
+const LoginPage = () => {
+  const handleLogin = ({ nome, email, senha }) => {
+    Axios.post('http://localhost:3001/register', {
+      nome,
+      email,
+      senha,
+    }).then((response) => {
+      alert(response.data.msg);
+    });
+  };
+
+  const validationsLogin = yup.object().shape({
+    nome: yup.string().required('O nome é obrigatório'),
+    email: yup.string().required('O email é obrigatório'),
+    senha: yup.string().required('A senha é obrigatória'),
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <PetsIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleLogin} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="nome"
+              label="Nome"
+              name="nome"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="email"
+              label="email"
+              id="email"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="senha"
+              label="Senha"
+              type="password"
+              id="senha"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Entrar
+            </Button>
+            <Grid container>
+              <Grid item>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default RegisterForm;
